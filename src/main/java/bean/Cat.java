27862a9d75f66f950c532421fbae53a4b32946a4
Alex Kherman <alexxerman@gmail.com>
2019-10-09 13:46:42 +0300

    1-st issue

--- src/main/java/bean/Cat.java
+++ src/main/java/bean/Cat.java
@@ -1,4 +1,48 @@
 package bean;
 
 public class Cat {
+    private int id;
+    private String name;
+    private Cat father;
+    private Cat mother;
+
+    public Cat(int id, String name, Cat father, Cat mother) {
+        this.id = id;
+        this.name = name;
+        this.father = father;
+        this.mother = mother;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Cat getFather() {
+        return father;
+    }
+
+    public void setFather(Cat father) {
+        this.father = father;
+    }
+
+    public Cat getMother() {
+        return mother;
+    }
+
+    public void setMother(Cat mother) {
+        this.mother = mother;
+    }
+
 }
